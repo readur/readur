c78fa4c8beabef52fb4d213f76e4d6d8b171701d
perf3ct <jonfuller2012@gmail.com>
2025-10-04 14:14:42 -0700

    feat(ui): add german and french translations

--- frontend/src/i18n/types.ts
+++ frontend/src/i18n/types.ts
@@ -1,6 +1,8 @@
 export const supportedLanguages = {
   en: 'English',
   es: 'Español',
+  de: 'Deutsch',
+  fr: 'Français',
 } as const;
 
 export type SupportedLanguage = keyof typeof supportedLanguages;
