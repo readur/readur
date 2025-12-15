54c032ef01e412fa34e8eff24258bcdc2a825659
perfectra1n <jonfuller2012@gmail.com>
2025-12-15 10:25:02 -0800

    feat(dev): increment version number

--- src/swagger.rs
+++ src/swagger.rs
@@ -178,7 +178,7 @@ use crate::{
     modifiers(&SecurityAddon),
     info(
         title = "Readur API",
-        version = "2.5.3",
+        version = "2.7.2",
         description = "Document management and OCR processing API",
         contact(
             name = "Readur Team",
