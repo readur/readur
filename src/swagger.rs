782de36ad98dea8c5104e056a997cd3c12d536f8
perfectra1n <jonfuller2012@gmail.com>
2026-03-08 10:47:31 -0700

    feat(dev): bump version

--- src/swagger.rs
+++ src/swagger.rs
@@ -178,7 +178,7 @@ use crate::{
     modifiers(&SecurityAddon),
     info(
         title = "Readur API",
-        version = "2.7.6",
+        version = "2.7.7",
         description = "Document management and OCR processing API",
         contact(
             name = "Readur Team",
